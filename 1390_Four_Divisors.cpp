0ba5cfc110d604d3f06ff85004e028dba7c2dbc7
Omkar52 <101172166+Omkar-githb@users.noreply.github.com>
2026-01-04 20:45:42 +0530

    Add files via upload

---
+++ 1390_Four_Divisors.cpp
@@ -0,0 +1,36 @@
+// 1390 Four Divisors
+
+class Solution {
+public:
+   
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+
+        for (int n : nums) {
+            int cnt = 0;
+            int sum = 0;
+
+            for (int i = 1; i * i <= n; i++) {
+                if (n % i == 0) {
+                    int d1 = i;
+                    int d2 = n / i;
+
+                    cnt++;
+                    sum += d1;
+
+                    if (d1 != d2) {
+                        cnt++;
+                        sum += d2;
+                    }
+
+                    if (cnt > 4) break;  // early stop
+                }
+            }
+
+            if (cnt == 4)
+                ans += sum;
+        }
+
+        return ans;
+    }
+};
